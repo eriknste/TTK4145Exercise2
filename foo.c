88df03a8f919a8ae5443c4b7985f3d058844db41
alekselvebakk <aleksnel@stud.ntnu.no>
2019-01-18 10:58:42 +0100

    Added files from exercise 1

---
+++ foo.c
@@ -0,0 +1,39 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int i = 0;
+
+// Note the return type: void*
+void* incrementingThreadFunction(){
+    // TODO: increment i 1_000_000 times
+    int j=0;
+    for (j; j<999999; j++){
+        i++;
+    }
+    return NULL;
+}
+
+void* decrementingThreadFunction(){
+    // TODO: decrement i 1_000_000 times
+    int j=0;
+    for (j; j<999999; j++){
+        i--;
+    }
+    return NULL;
+}
+
+
+int main(){
+    // TODO: declare incrementingThread and decrementingThread (hint: google pthread_create)
+    pthread_t incrementingThread;
+    pthread_t decrementingThread;
+    
+    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
+    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
+    
+    pthread_join(incrementingThread, NULL);
+    pthread_join(decrementingThread, NULL);
+    
+    printf("The magic number is: %d\n", i);
+    return 0;
+}
